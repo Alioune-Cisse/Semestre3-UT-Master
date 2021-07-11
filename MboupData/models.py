9c29f49958396c526fedef8782a80af36dccc655
Alioune CISSE <60492447+Alioune-Cisse@users.noreply.github.com>
2021-07-11 13:43:09 +0000

    Update models.py

--- MboupData/models.py
+++ MboupData/models.py
@@ -61,7 +61,7 @@ class LiverModel(pl.LightningModule):
     def add_model_specific_args(parent_parser):
        parser = parent_parser.add_argument_group("LiverModel")
        parser.add_argument('--encoder_layers', type=int, default=12)
-       parser.add_argument('--data_path', type=str, default='/some/path')
+       parser.add_argument('--data_path', type=str, default='/content/')
        return parent_parser
     
     def forward(self, x):
